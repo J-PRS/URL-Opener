ed7d757993ba7b5bc4201770c4cb04a41bf8406b
JPrice <138972163+J-PRS@users.noreply.github.com>
2024-07-10 13:04:14 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,10 @@ def format_line_with_style(text, top_margin = "inherit", bottom_margin = "inheri
 
 url = st.text_input("URL", label_visibility="collapsed")
 st.markdown(f"[APP]({url})")
+
+st.markdown(
+    f'<a href="{url}" style="display: inline-block; padding: 12px 20px; background-color: #4CAF50; color: white; text-align: center; text-decoration: none; font-size: 16px; border-radius: 4px;">Action Text on Button</a>',
+    unsafe_allow_html=True
+)
+
 webbrowser.open(url)
