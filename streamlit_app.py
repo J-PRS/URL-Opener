14d0a8d0308dc49433fa00de80edca577e077c25
JPrice <138972163+J-PRS@users.noreply.github.com>
2024-07-10 13:06:33 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import streamlit.components.v1 as components
 import webbrowser
 
 def format_line_with_style(text, top_margin = "inherit", bottom_margin = "inherit", font_size = 24):
@@ -13,5 +14,19 @@ st.markdown(
     f'<a href="{url}" style="display: inline-block; padding: 12px 20px; background-color: #4CAF50; color: white; text-align: center; text-decoration: none; font-size: 16px; border-radius: 4px;">APP</a>',
     unsafe_allow_html=True
 )
+
+components.html(
+f"""
+    <div>some hidden container</div>
+    <p>{st.session_state.counterText}</p>
+    <script>
+        var input = window.parent.document.querySelectorAll("[type=text]");
+        for (var i = 0; i < input.length; ++i) {{
+            input[i].setAttribute('inputmode', 'numeric');
+            input[i].focus();
+        }}
+    </script>
+""",
+height=0)
 
 webbrowser.open(url)
