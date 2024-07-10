97e44600155fbc37af5201f3579d7220e74eab91
JPrice <138972163+J-PRS@users.noreply.github.com>
2024-07-10 13:15:27 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,6 @@ import streamlit as st
 import streamlit.components.v1 as components
 import webbrowser
 
-def format_line_with_style(text, top_margin = "inherit", bottom_margin = "inherit", font_size = 24):
-    style = f"text-align: center; margin-top: {top_margin}px; margin-bottom: {bottom_margin}px; font-size: {font_size}px;"
-    line = f"<p style='{style}'>{text}</p>"
-    st.markdown(line, unsafe_allow_html=True)
-
 url = st.text_input("URL", label_visibility="collapsed")
 st.markdown(f"[APP]({url})")
 
@@ -18,15 +13,9 @@ st.markdown(
 components.html(
 f"""
     <div>some hidden container</div>
-    <p>{url}</p>
+    <p>{st.session_state.counterText}</p>
     <script>
-        var input = window.parent.document.querySelectorAll("[type=text]");
-        for (var i = 0; i < input.length; ++i) {{
-            input[i].setAttribute('inputmode', 'numeric');
-            input[i].focus();
-        }}
+        document.querySelector("a[href]").click();
     </script>
 """,
 height=0)
-
-webbrowser.open(url)
