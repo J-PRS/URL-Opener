bc0bbb35e79c063df4541703381c8deb8cab102c
JPrice <138972163+J-PRS@users.noreply.github.com>
2024-07-10 12:58:43 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 import webbrowser
 
+def format_line_with_style(text, top_margin = "inherit", bottom_margin = "inherit", font_size = 24):
+    style = f"text-align: center; margin-top: {top_margin}px; margin-bottom: {bottom_margin}px; font-size: {font_size}px;"
+    line = f"<p style='{style}'>{text}</p>"
+    st.markdown(line, unsafe_allow_html=True)
+
 url = st.text_input("URL", label_visibility="collapsed")
-st.markdown(f"[APP]({url})")
+format_line_with_style(f"[APP]({url})")
 webbrowser.open(url)
