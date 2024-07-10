e9639dfc7c12b7406305b1cac060b3f672d291d8
JPrice <138972163+J-PRS@users.noreply.github.com>
2024-07-10 13:52:21 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,29 +5,17 @@ import webbrowser
 url = st.text_input("URL", label_visibility="collapsed")
 
 if "http" in url:
-    # st.markdown(f"[APP]({url})")
-    # st.write(url)
-    # st.markdown()
     st.markdown(
-        f'<a href="{url}" style="display: inline-block; padding: 12px 20px; background-color: #4CAF50; color: white; text-align: center; text-decoration: none; font-size: 16px; border-radius: 4px;">APP</a>',
+        f'<a href="{url}" target="_self">APP</a>',
         unsafe_allow_html=True
     )
 
     components.html(
     f"""
-        <div>
-        <p>
-        <a href="https://copilot.microsoft.com/" target="_self" rel="noopener noreferrer">APP</a>
-        </p>
-        </div>
-    """,
-    height=0)
-
-    components.html(
-    f"""
         <div>some hidden container</div>
         <script>
-            window.parent.document.querySelector("a[href]").click();
+            var button = window.parent.document.querySelector("a[href]");
+            button.click()
         </script>
     """,
     height=0)
