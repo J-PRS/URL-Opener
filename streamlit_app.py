2d0869a0cba9a803bac7887391588d26d99fcdb4
JPrice <138972163+J-PRS@users.noreply.github.com>
2024-07-10 13:18:26 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,18 +3,20 @@ import streamlit.components.v1 as components
 import webbrowser
 
 url = st.text_input("URL", label_visibility="collapsed")
-st.markdown(f"[APP]({url})")
 
-st.markdown(
-    f'<a href="{url}" style="display: inline-block; padding: 12px 20px; background-color: #4CAF50; color: white; text-align: center; text-decoration: none; font-size: 16px; border-radius: 4px;">APP</a>',
-    unsafe_allow_html=True
-)
+if "http" in url:
+    st.markdown(f"[APP]({url})")
 
-components.html(
-f"""
-    <div>some hidden container</div>
-    <script>
-        document.querySelector("a[href]").click();
-    </script>
-""",
-height=0)
+    st.markdown(
+        f'<a href="{url}" style="display: inline-block; padding: 12px 20px; background-color: #4CAF50; color: white; text-align: center; text-decoration: none; font-size: 16px; border-radius: 4px;">APP</a>',
+        unsafe_allow_html=True
+    )
+
+    components.html(
+    f"""
+        <div>some hidden container</div>
+        <script>
+            document.querySelector("a[href]").click();
+        </script>
+    """,
+    height=0)
